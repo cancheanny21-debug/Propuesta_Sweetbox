b4d0e46d232b15c57492267329ec2224acb9e6d5
Anny Canche <cancheanny21@gmail.com>
2026-07-27 15:14:42 -0500

    bd s1

--- sweetbox_db.sql
+++ sweetbox_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-07-2026 a las 23:15:11
+-- Tiempo de generación: 27-07-2026 a las 21:51:20
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
